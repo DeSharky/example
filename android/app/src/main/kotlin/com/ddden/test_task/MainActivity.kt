10417fe437224ebb08ff89890b43ee33ca592b64
DeSharky <chukasta@ya.ru>
2022-01-29 16:47:23 +0300

    initial commit

---
+++ android/app/src/main/kotlin/com/ddden/test_task/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.desharky.currencies
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:currencies/home/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Currencies list',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const HomeScreen(),
+    );
+  }
+}
